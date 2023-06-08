46cc69dc9e12c298ec2a63251262c428723325fb
freitzzz <1160907@isep.ipp.pt>
2023-06-08 19:39:21 +0100

    fix: example

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,4 @@
-import { default as upsert, GitHubRepository } from 'github-upsert';
+import { default as upsert, GitHubRepository, File } from 'github-upsert';
 
 async function main() {
 	// You can grab your personal access token in: Settings > Developer Settings > Personal Access Tokens
@@ -9,10 +9,12 @@ async function main() {
 	};
 
 	const data = new TextEncoder().encode('Hello world!');
+	const file = <File>{ data: data }
+
 	const path = 'README.md';
 
 	// Upload it
-	const result = await upsert(repo, data, path);
+	const result = await upsert(repo, file, path);
 
 	// Hoooraaaay! It should print the SHA checksum of your file!
 	console.log(result);
